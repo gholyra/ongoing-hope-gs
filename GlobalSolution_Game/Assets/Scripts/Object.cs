97e18089416442fee009b518e73368da608a626a
Tiago Marchetti Lyra <tiago.mlyra@gmail.com>
2023-05-30 21:40:32 -0300

    Criação de raycasting do objeto interativo
    Implementação da base do sistema de interação a partir do cursor do mouse.

--- GlobalSolution_Game/Assets/Scripts/Object.cs
+++ GlobalSolution_Game/Assets/Scripts/Object.cs
@@ -4,17 +4,40 @@ using UnityEngine;
 
 public class Object : MonoBehaviour
 {
+
+    private Transform itemTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        itemTransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RayCastObject();
     }
 
-   
+    private void RayCastObject()
+    {
+        Debug.DrawRay(GetMousePosition(), -Vector3.up * 10, Color.white, 0);
+        RaycastHit2D hit = Physics2D.Raycast(GetMousePosition(), -Vector2.up, LayerMask.GetMask("Object"));
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject.tag == "InteractiveObject")
+            {
+                Debug.Log("Hovering");
+            }
+        }
+    }
+
+    private Vector3 GetMousePosition()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mousePosition.z = 0f;
+        return mousePosition;
+    }
+
+
 }
